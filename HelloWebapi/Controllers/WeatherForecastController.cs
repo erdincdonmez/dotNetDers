45f11b793219a313db58b6595e3920192fd0264f
erdincDonmez <erdinc_donmez@yahoo.com>
2022-06-03 22:37:16 +0300

    api-1

--- HelloWebapi/Controllers/WeatherForecastController.cs
+++ HelloWebapi/Controllers/WeatherForecastController.cs
@@ -3,7 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 namespace HelloWebapi.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+//[Route("[controller]")] //orjinali bu idi
+[Route("api/[controller]s")]
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
@@ -18,8 +19,32 @@ public class WeatherForecastController : ControllerBase
         _logger = logger;
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
+    [HttpGet(Name = "GetWeatherForecast")] // orjinal action method
     public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+    [HttpGet] // overload gibi ekleme yapıldı
+    // api/WeatherForecasts?id=3 // gibi ulaşılabilen bir metod oldu.
+    public IEnumerable<WeatherForecast> GetById([FromQuery] string id)
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+    [HttpGet("{id}")] // overload gibi ekleme yapıldı
+    // api/WeatherForecasts/3 // gibi ulaşılabilen bir metod oldu.
+    public IEnumerable<WeatherForecast> GetForecast(string id)
     {
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
