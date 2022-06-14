b199af71a5a2922f6ec51b9d7ebc33ab36533aee
erdincDonmez <erdinc_donmez@yahoo.com>
2022-06-14 23:08:25 +0300

    DB Context

---
+++ DenemeWebApi/DBOperations/DataGenerator.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DenemeWebApi.DBOperations
+{
+    public class DataGenerator
+    {
+        public static void Initialize(IServiceProvider serviceProvider){
+            using (var context = new BookStoreDBContext(serviceProvider.GetRequiredService<DbContextOptions<BookStoreDBContext>>()))
+            {
+                if (context.Books.Any()) return;
+                context.Books.AddRange(
+                    new Book{
+                        Id = 1,
+                        Title = "Deneme Kitap Başlığı1",
+                        GenreId = 3,
+                        PageCount = 100,
+                        PublishDate = new DateTime(2022,05,06),
+                    },
+                    new Book{
+                        Id = 4,
+                        Title = "Deneme Kitap Başlığı44",
+                        GenreId = 1,
+                        PageCount = 200,
+                        PublishDate = new DateTime(2022,05,06),
+                    },
+                    new Book{
+                        Id = 3,
+                        Title = "Deneme Kitap Başlığı1",
+                        GenreId = 4,
+                        PageCount = 300,
+                        PublishDate = new DateTime(2022,05,06),
+                    }
+
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- DenemeWebApi/Program.cs
+++ DenemeWebApi/Program.cs
@@ -1,5 +1,7 @@
+using DenemeWebApi.DBOperations;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,7 +15,15 @@ namespace DenemeWebApi
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var Services = scope.ServiceProvider;
+                DataGenerator.Initialize(Services);
+            }
+            host.Run();
+            
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
